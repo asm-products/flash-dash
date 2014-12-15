d747a52aec9bc0ab871663d3c97551f69fc1436f
catalinmiron <mironcatalin@gmail.com>
2014-12-16 00:52:59 +0200

    Add new heroku config

--- src/app.js
+++ src/app.js
@@ -1,16 +1,5 @@
-var express = require('express');
+'use strict';
 
-var path = require('path');
-
-app = express();
-
-app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(express.static(path.join(__dirname, 'dist')));
-});
-
-var port = process.env.PORT || 5000;
-
-app.listen(port);
-
-console.log('server started '+port);
+var statik = require('statik');
+var server = statik.createServer('.');
+server.listen();
